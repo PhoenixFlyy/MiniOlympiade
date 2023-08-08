d276031d6dbfbb0298b5a46e52fd5808a5adba6e
Felix <Felix.Scheuerpflug@gmail.com>
2023-08-08 18:35:00 +0200

    Initialize & Connect Firebase and Realtime Database

--- winter_olympiade/lib/main.dart
+++ winter_olympiade/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -5,8 +6,9 @@ import 'package:winter_olympiade/MainMenu.dart';
 
 import 'TeamSelection.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     runApp(const MyApp());
