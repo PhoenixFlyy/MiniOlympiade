9cf89453ab6b258a4cae6379531a309dda3e972f
Felix <Felix.Scheuerpflug@gmail.com>
2024-08-19 23:14:24 +0200

    move assets and adjust config

--- winter_olympiade/lib/wuecade/game/assets.dart
+++ winter_olympiade/lib/wuecade/game/assets.dart
@@ -1,19 +1,19 @@
 class Assets {
-  static const background = 'assets/wuecade/images/background.png';
-  static const ground = 'assets/wuecade/images/ground.png';
-  static const clouds = 'assets/wuecade/images/clouds.png';
-  static const pipe = 'assets/wuecade/images/pipe.png';
-  static const pipeRotated = 'assets/wuecade/images/pipe_rotated.png';
+  static const background = 'background.png';
+  static const ground = 'ground.png';
+  static const clouds = 'clouds.png';
+  static const pipe = 'pipe.png';
+  static const pipeRotated = 'pipe_rotated.png';
 
-  static const birdMidFlap = 'assets/wuecade/images/bird_midflap.png';
-  static const birdUpFlap = 'assets/wuecade/images/bird_upflap.png';
-  static const birdDownFlap = 'assets/wuecade/images/bird_downflap.png';
+  static const birdMidFlap = 'bird_midflap.png';
+  static const birdUpFlap = 'bird_upflap.png';
+  static const birdDownFlap = 'bird_downflap.png';
 
-  static const gameOver = 'assets/wuecade/images/gameover.png';
-  static const menu = 'assets/wuecade/images/menu.jpg';
-  static const message = 'assets/wuecade/images/message.png';
+  static const gameOver = 'assets/images/gameover.png';
+  static const menu = 'assets/images/menu.jpg';
+  static const message = 'assets/images/message.png';
 
-  static const flying = 'assets/wuecade/audio/fly.wav';
-  static const collision = 'assets/wuecade/audio/collision.wav';
-  static const point = 'assets/wuecade/audio/point.wav';
+  static const flying = 'fly.wav';
+  static const collision = 'collision.wav';
+  static const point = 'point.wav';
 }
--- winter_olympiade/lib/wuecade/game/configuration.dart
+++ winter_olympiade/lib/wuecade/game/configuration.dart
@@ -1,7 +1,7 @@
 class Config {
   static const gameSpeed = 200.0;
-  static const groundHeight = 110.0;
-  static const pipeInterval = 1.5;
+  static const groundHeight = 65.0;
+  static const pipeInterval = 1.75;
   static const birdVelocity = 250;
-  static const gravity = -100.0;
+  static const gravity = -110.0;
 }
