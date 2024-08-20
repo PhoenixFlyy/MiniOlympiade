1096cc01cf54af7aba3f733fbd51694168156862
Felix <Felix.Scheuerpflug@gmail.com>
2024-08-20 11:38:14 +0200

    better settings #3

--- winter_olympiade/lib/wuecade/game/configuration.dart
+++ winter_olympiade/lib/wuecade/game/configuration.dart
@@ -2,6 +2,6 @@ class Config {
   static const gameSpeed = 200.0;
   static const groundHeight = 65.0;
   static const pipeInterval = 1.75;
-  static const birdVelocity = 300;
-  static const gravity = -125.0;
+  static const birdVelocity = 400;
+  static const gravity = -150.0;
 }
