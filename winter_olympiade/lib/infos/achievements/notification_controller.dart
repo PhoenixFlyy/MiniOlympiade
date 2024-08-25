7b06a5d5f7e535d344d8b05c9bfa091c0282b039
sim32101 <140886890+sim32101@users.noreply.github.com>
2024-08-25 18:49:54 +0200

    Klick auf Benachrichtigung führt zu Ach-Screen

--- winter_olympiade/lib/infos/achievements/notification_controller.dart
+++ winter_olympiade/lib/infos/achievements/notification_controller.dart
@@ -1,24 +1,27 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:olympiade/main.dart';  // Stelle sicher, dass du den richtigen Pfad zur MyApp-Klasse hast
 
 class NotificationController {
   @pragma("vm:entry-point")
   static Future<void> onNotificationCreatedMethod(
-      ReceivedNotification receivedNotification) async {
-  }
+      ReceivedNotification receivedNotification) async {}
 
   @pragma("vm:entry-point")
   static Future<void> onNotificationDisplayedMethod(
-      ReceivedNotification receivedNotification) async {
-  }
+      ReceivedNotification receivedNotification) async {}
 
   @pragma("vm:entry-point")
   static Future<void> onDismissActionReceivedMethod(
-      ReceivedAction receivedAction) async {
-  }
+      ReceivedAction receivedAction) async {}
 
   @pragma("vm:entry-point")
   static Future<void> onActionReceivedMethod(
       ReceivedAction receivedAction) async {
+    // Hier wird überprüft, ob es sich um eine Achievement-Benachrichtigung handelt
+    if (receivedAction.channelKey == 'achievement_channel') {
+      // Navigiere zur Achievement-Seite
+      MyApp.navigatorKey.currentState?.pushNamed('/achievements');
+    }
   }
-
 }
